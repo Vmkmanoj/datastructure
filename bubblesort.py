5ddb5c77795fee3006713e95014e538ae79c2032
manojkumar <116948057+Vmkmanoj@users.noreply.github.com>
2023-04-28 23:22:39 -0700

    Add files via upload

---
+++ bubblesort.py
@@ -0,0 +1,23 @@
+def bubblesort(elements,key=None):
+    size=len(elements)
+    for i in range(size-1):
+        for j in range(size-1-i):
+            a=elements[j][key]
+            b=elements[j+1][key]
+            if a>b:
+                temb=elements[j]
+                elements[j]=elements[j+1]
+                elements[j+1]=temb
+                
+                
+if __name__=="__main__":
+    elements = [
+        { 'name': 'mona',   'transaction_amount': 1000, 'device': 'iphone-10'},
+        { 'name': 'dhaval', 'transaction_amount': 400,  'device': 'google pixel'},
+        { 'name': 'kathy',  'transaction_amount': 200,  'device': 'vivo'},
+        { 'name': 'aamir',  'transaction_amount': 800,  'device': 'iphone-8'},
+    ]
+    
+    bubblesort(elements,key="transaction_amount")
+    
+    print(elements)
